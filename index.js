14de5a5cab41ac90a1697c45fa38fa69ca72c095
Huynh Huu Khang <huukhang1999@gmail.com>
2023-01-08 15:06:56 +0700

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function (req, res) {
+  return res.send('Hello world');
+})
+
+console.log('log info');
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
