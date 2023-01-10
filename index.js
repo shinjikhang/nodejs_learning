eb6268dbebe4b1faabd559907fae016bb2d3c9da
Huynh Huu Khang <huukhang1999@gmail.com>
2023-01-10 21:13:54 +0700

    Add lib morgan dev

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
@@ -6,7 +7,7 @@ app.get('/', function (req, res) {
   return res.send('Hello world');
 })
 
-console.log('log info');
+app.use(morgan('combined'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
