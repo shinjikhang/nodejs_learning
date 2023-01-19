015199190d897e07760abf1e5a1fc8e71c61566e
Huynh Huu Khang <huukhang1999@gmail.com>
2023-01-19 14:42:29 +0700

    Update array congratulation content

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
+const path = require('path');
 const express = require('express')
 const morgan = require('morgan')
-const handlebars = require("handlebars");
+const { engine } = require('express-handlebars');
 const app = express()
 const port = 3000
 
 // setup the logger
 app.use(morgan('combined'))
 
-app.get('/', function (req, res) {
-  res.send('hello, world!')
-})
 
-//template engine
-app.engine('handlebars', handlebars());
-//use handlebars
-app.set('view engine', 'handlebars');
+app.engine('hbs', engine({
+  extname: ".hbs"
+}));
+app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/tin-tuc', (req, res) => {
+  res.render('tin-tuc');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
