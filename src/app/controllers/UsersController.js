5500f467a0e637e9f97b5e8e759f92c928e93d2b
Huynh Huu Khang <huukhang1999@gmail.com>
2023-02-07 14:41:45 +0700

    Update UserController

--- src/app/controllers/UsersController.js
+++ src/app/controllers/UsersController.js
@@ -2,23 +2,28 @@ const User = require('../models/User')
 class UsersController {
     //[GET] /users
     index(req, res, next) {
-        // User.find({}, function (err, docs) {
-        //     if (err) {
-        //         return res.send(docs);
-        //     }
-        //     next(err);
-        //     res.status(400).json({ error: 'message' })
-        // });
-
         User.find({})
-            .then(user => res.json(user))
+            .then(users => res.json({
+                users: users
+            }))
             .catch(error => next(error));
-
     }
 
     //[GET] /users/:slug
     show(req, res) {
-        res.render("user-show");
+        const userId = req.params.id;
+        // using callback
+        User.findById(userId, function (err, user) {
+            if (err) {
+                res.status(500).json({ message: err });
+            } else {
+                if (!user) {
+                    return res.status(404).json({ message: 'User not found' });
+                } else {
+                    return res.status(200).json(user)
+                }
+            }
+        });
     }
 
     //insert
